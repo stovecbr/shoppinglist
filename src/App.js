c1177e62d89538a4e8a1c7ef15b2cd712f0cd90d
stovecbr <pc-018@st-ove.co.jp>
2022-10-26 14:01:49 +0900

    プラスボタンでアイテム追加

--- src/App.js
+++ src/App.js
@@ -4,49 +4,74 @@ import React, { useState } from 'react';
 
 const App = () => {
 
-  // const[items,setItems]= useState([]);
+  
   const[count,setCount]=useState(0);
+  const[items,setItems]= useState("");
 
+  const[shoppingList,setShoppingList] = useState(["item1","item2"]);
+  
+  // ＜ボタンクリック
   const handleClickDown =()=>{
     const newCount = count-1;
     setCount(newCount);
   };
-
+  // ＞ボタンクリック
   const handleClickUp =()=>{
     const newCount = count+1;
     setCount(newCount);
   };
 
+  // ＋ボタンクリック
+  const onCLickAdd = () =>{
+    if (items === "") return;
+    const newItems = [...shoppingList,items];
+    setShoppingList(newItems);
+    setItems("");
+  }
+
+  // 入力した値を反映する
+  const onChangeItems = (event) =>setItems(event.target.value);
+
 
   return (
     <div className="App">
       {/* <ShoppingList items={items} /> */}
+
       <div className='input-area'>
-        <input type='text'  placeholder='Add an item...'></input>
-        <button>+</button> 
+        <input
+          placeholder='Add an item...'
+          value={items}
+          onChange={onChangeItems}
+          ></input>
+
+        <button onClick={onCLickAdd}>+</button> 
       </div>
       
       <div className='shopping-list'>
-        
-          <input type="checkbox"></input>       
-            item1  
-          <button onClick={handleClickDown}>＜</button>
-            {count}
-          <button onClick={handleClickUp}>＞</button>
-       
+
+           
+      <ul>
+        {shoppingList.map((item,index)=>{
+          return(
+            
+            <div key={index} className="list-row">
+              
+                <li>
+                  <input type="checkbox" ></input>
+                    {item}
+                  <button onClick={handleClickDown}>＜</button>
+                    {count}
+                  <button onClick={handleClickUp}>＞</button>
+                </li>
+              
+            </div>
+            
+          );
+        })}
+
+      </ul>
         
       </div>
-      <div className='shopping-list'>
-        
-          <input type="checkbox"></input>       
-            item2  
-          <button>＜</button>
-            2個
-          <button>＞</button>
-       
-        
-      </div>
-   
 
     </div>
   );
